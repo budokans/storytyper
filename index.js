df129f31224c776ff53ebc9a8df55d7af41f3766
budokans <68995729+budokans@users.noreply.github.com>
2021-02-19 15:33:08 +1300

    Fixed TypeError and promise rejection issues
    Now scraping multiple pages recursively with no promise rejection errors!

--- index.js
+++ index.js
@@ -1,28 +1,44 @@
 import express from "express";
-import getStoriesData from "./lib/scraper";
+import { scrapeStories } from "./lib/scraper";
 import db from "./lib/db";
 const hostname = "127.0.0.1";
-const port = 2093;
+const port = 2094;
 
 const app = express();
 
+// // const lastStoryId = db.get(`stories[0].id`).value();
+// async function go() {
+//   const storiesPromise = getStoriesData(
+//     "http://fiftywordstories.com/category/stories/"
+//   );
+//   const [stories, scrapeCount] = await storiesPromise;
+//   console.log(storiesPromise);
+//   console.log(stories, scrapeCount);
+// }
+
+// go();
+
 app.get("/scrape", async (req, res, next) => {
   console.log("Scraping!");
-  const stories = await getStoriesData(
-    "http://fiftywordstories.com/category/stories/"
-  );
 
-  console.log("Scraping complete!");
+  scrapeStories("http://fiftywordstories.com/category/stories/")
+    .then((result) => result)
+    .then((storiesData) => {
+      const { stories, scrapeCount, pageCount } = storiesData;
+      console.log(
+        `Scraping complete: ${scrapeCount} stories successfully scraped over ${pageCount} pages`
+      );
+      stories.forEach((story) => {
+        story.dateScraped = Date.now();
 
-  stories.forEach((story) => {
-    story.dateScraped = Date.now();
+        // Check if the story has an author. If not, push to storiesToEdit array
+        story.author
+          ? db.get("stories").push(story).write()
+          : db.get("storiesToEdit").push(story).write();
+      });
 
-    story.author
-      ? db.get("stories").push(story).write()
-      : db.get("storiesToEdit").push(story).write();
-  });
-
-  res.json(stories);
+      res.json(stories);
+    });
 });
 
 app.listen(port, () => {
