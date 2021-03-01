eaf059e8ae83b6270073da64593ffaddc5888d84
budokans <68995729+budokans@users.noreply.github.com>
2021-03-02 01:35:57 +1300

    Story Formatting algorithm optimized
    AGAIN! Issues with no spaces between punctuation marks and opening quotes/parentheses etc fixed.
    
    Max-height of story-box also expanded due to the poetic formatting of several new stories.

--- backend/lib/formatStoryText.js
+++ backend/lib/formatStoryText.js
@@ -9,7 +9,10 @@ export default function formatStoryText(text) {
     .replace(/  +/g, " ")
     // Remove spaces between end of words and punctuation marks
     .replace(/\s\W/g, (match) => {
-      return match.replace(/\s/, "");
+      // If the punctuation mark following the space is a wrapping punctuation mark, do nothing, otherwise remove the space
+      const wrappingPuncFollowsSpace =
+        match.charAt(1) === "'" || '"' || "(" || ")";
+      return !wrappingPuncFollowsSpace ? match.replace(/\s/, "") : match;
     })
     .trim();
 
@@ -21,3 +24,5 @@ export default function formatStoryText(text) {
     .replace(/’/g, "'")
     .replace(/—/g, " - ");
 }
+
+// C'est magnifique , this thing called love. Since we met, you do something to me and anything goes. You're easy to love so let's misbehave night and day. Blow, Gabriel, blow! It's just one of those things: we shall never be younger. Come along with me. How could we be wrong?
