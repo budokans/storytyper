3b21d369c25e41b7c90beeb33a67d79bcd074c67
budokans <68995729+budokans@users.noreply.github.com>
2021-03-03 13:43:54 +1300

    mongoDB connection working
    Cluster created and is accessible from Node app.

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,37 @@
 import express from "express";
+import { MongoClient } from "mongodb";
 import cors from "cors";
 import { scrapeStories } from "./lib/scraper";
 import "./lib/cron";
 import db from "./lib/db";
+
+// mongoDB stuff
+const uri =
+  "mongodb+srv://swebster:k0dov8yRH*yUI0&3uf@cluster0.p2rcx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const client = MongoClient(uri, { useUnifiedTopology: true });
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db("sample_mflix");
+    const movies = database.collection("movies");
+
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { title: "Back to the Future" };
+    const movie = await movies.findOne(query);
+
+    console.log(movie);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+// Express and route handlers stuff
 
 const hostname = "127.0.0.1";
 const port = 2094;
