a7b20c9c1293875d44574dea01ae0487b0276b98
budokans <68995729+budokans@users.noreply.github.com>
2021-03-05 00:21:56 +1300

    Created db config file
    and imported the properties into db.js for use in the uri.

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
+import config from "../conf/mongoConfig";
 
-const uri =
-  "mongodb+srv://swebster:k0dov8yRH*yUI0&3uf@cluster0.p2rcx.mongodb.net/storytyper?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${config.user}:${config.pass}@${config.cluster}.mongodb.net/storytyper?retryWrites=true&w=majority`;
 
 let client = null;
 
