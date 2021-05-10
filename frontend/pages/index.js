9cb8da6e667e6c3ed227e1d6395ce4fc5b03e307
budokans <68995729+budokans@users.noreply.github.com>
2021-05-11 00:09:20 +1200

    Improve favicon and favicon/icon delivery

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -16,6 +16,24 @@ export default function Home() {
           href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css"
           rel="stylesheet"
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
         <meta
           name="description"
           content="A desktop speed-typing game based around fifty-word stories written by authors from all walks of life."
