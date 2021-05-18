fadc52da4df910aae042b05fbe8baeca7e926b72
budokans <68995729+budokans@users.noreply.github.com>
2021-05-18 14:25:58 +1200

    Optimize font-loading
    Block font-display until loaded.

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -13,10 +13,6 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Speed Typer</title>
         <link
-          href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap"
-          rel="stylesheet"
-        />
-        <link
           href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css"
           rel="stylesheet"
         />
